8789742ced421e9a5f6c3f1f1b532991b864ef0c
Jacob M. Lundeen <jacob.lundeen@gmail.com>
2023-04-26 15:56:07 -0400

    Continued work on learn_model() function.

--- main.py
+++ main.py
@@ -47,17 +47,75 @@ def generate_data(data, n):
     return result
 
 
+def softmax(Z: List) -> List:
+    A = np.exp(Z) / sum(np.exp(Z))
+    return A
+
+
+def sigmoid(z):
+    return 1.0 / (1 + np.exp(z))
+
+
+def forward_prop(x_data: List[List], w1: List, b1: List, w2: List, b2: List) -> Tuple:
+    z1 = w1.dot(x_data.T) + b1
+    a1 = sigmoid(z1)
+    z2 = w2.dot(a1) + b2
+    a2 = softmax(z2)
+    return z1, a1, z2, a2
+
+
+def calculate_error(y_data: List, a2: List) -> float:
+    n = len(y_data)
+    temp = np.subtract(1, a2)
+    temp2 = np.log(temp, out=temp, where=temp > 0)
+    error = (-1 / n) * np.sum(y_data * np.log(a2.T) + np.subtract(1, y_data) * temp2.T)
+    return error
+
+
+def sigmoid_deriv(z):
+    return z * (1 - z)
+
+
+def backward_prop(z1, a1, z2, a2, w1, w2, x_data, y_data):
+    m, _ = x_data.shape
+    dz2 = a2 - y_data.T
+    dw2 = 1 / m * dz2.dot(a1.T)
+    db2 = 1 / m * np.sum(dz2, axis=1)
+    dz1 = w2.T.dot(dz2) * sigmoid_deriv(z1)
+    dw1 = 1 / m * dz1.dot(x_data)
+    db1 = 1 / m * np.sum(dz1, axis=1)
+    return dw1, db1, dw2, db2
+
+
+def update_params(w1, b1, w2, b2, dw1, db1, dw2, db2, alpha):
+    w1 -= alpha * dw1
+    b1 -= alpha * np.expand_dims(db1, axis=1)
+    w2 -= alpha * dw2
+    b2 -= alpha * np.expand_dims(db2, axis=1)
+    return w1, b1, w2, b2
+
+
 def learn_model(data, hidden_nodes, verbose=False):
     x_data = data[:, :-4]
     y_data = data[:, -4:]
+    m, n = x_data.shape
     num_classes = len(y_data[0])
-    w1 = np.random.rand(hidden_nodes)
-    b1 = np.random.rand(hidden_nodes)
-    w2 = np.random.rand(num_classes)
-    b2 = np.random.rand(num_classes)
-    epsilon, alpha, previous_error = 1E-07, 0.1, 0.0
+    w1 = np.random.rand(hidden_nodes, n)
+    b1 = np.random.rand(hidden_nodes, 1)
+    w2 = np.random.rand(num_classes, hidden_nodes)
+    b2 = np.random.rand(num_classes, 1)
+    epsilon, alpha, previous_error = 1E-07, 0.01, 0.0
+    current_error = 1
+    count = 0
     while abs(current_error - previous_error) > epsilon:
-    pass
+        z1, a1, z2, a2 = forward_prop(x_data, w1, b1, w2, b2)
+        dw1, db1, dw2, db2 = backward_prop(z1, a1, z2, a2, w1, w2, x_data, y_data)
+        w1, b1, w2, b2 = update_params(w1, b1, w2, b2, dw1, db1, dw2, db2, alpha)
+        previous_error = current_error
+        current_error = calculate_error(y_data, a2)
+        count += 1
+    print(count)
+    return a2
 
 
 # This function is to convert the data structure from generate_data() into an easier to use numpy matrix
@@ -102,4 +160,4 @@ if __name__ == "__main__":
     data = generate_data(clean_data, 100)
     datum = transform_data(data)
     model = learn_model(datum, 2)
-    print(datum)
+    print(model)
